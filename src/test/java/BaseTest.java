9ace894b78bba9facc63989f8788b5fffa5f5af6
Roman Gurov <romario.gurov@gmail.com>
2024-12-31 14:03:24 +0300

    Added universal method for pressing notes. Increased optimization, added the possibility of extensibility.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,16 +4,26 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import pages.LobbyPage;
 import pages.StartPage;
 
 import java.time.Duration;
+import java.util.Map;
 
 public abstract class BaseTest {
 
     protected static ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(() -> {
         WebDriverManager.chromedriver().setup();
-        WebDriver d = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.setExperimentalOption("prefs", Map.of(
+                "profile.default_content_setting_values.geolocation", 2,
+                "profile.default_content_setting_values.notifications", 2,
+                "profile.default_content_setting_values.media_stream", 2,
+                "profile.default_content_setting_values.midi_sysex", 2
+        ));
+        WebDriver d = new ChromeDriver(options);
         d.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
         d.manage().window().maximize();
         return d;
